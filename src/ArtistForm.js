ae20c69ef66fb010a2d7dc7935938fa547388bd8
kristinmc42 <mccollum.k.j@gmail.com>
2021-11-19 22:42:05 -0500

    added API call to artistform

--- src/ArtistForm.js
+++ src/ArtistForm.js
@@ -1,13 +1,17 @@
 // gets the user's choice of artist
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 
 function ArtistForm(){
   // initialize useState for user input
   const [userInput, setUserInput] = useState("");
 
+  const [artistTopTracks, setArtistTopTracks] = useState([]);
+
+  // when user types something in the input field
   const handleChange = (event) => {
-    // update userInput state with what user types in input
+    // update userInput state with the input value
     setUserInput(event.target.value);
   }
 
@@ -20,12 +24,32 @@ function ArtistForm(){
 
     console.log(artistName)
 
+    // call API based on artist name 
+    const apiKey = "55a0c662e159f9a95c530a23f4af3da8";
+    // const sharedSecret = "5653c82e28f07d2107a042a3d733d57f";
+    axios({
+      url: "http://ws.audioscrobbler.com/2.0/",
+      params: {
+        format: "json",
+        method: "artist.gettoptracks",
+        api_key: apiKey,
+        artist: userInput,
+        autocorrect: [0 | 1]
+      }
+    })
+    .then(response => {
+      // 
+      console.log(response.data.toptracks.track[0].name, response.data.toptracks.track)
+
+      setArtistTopTracks(response.data.toptracks.track);
+    })
+
     // clear value of userInput
     setUserInput("");
   }
 
   return(
-  
+    <>
     <form onSubmit={handleSubmit}>
       <label htmlFor="artistChoice">Who is your musical inspiration today?</label>
       <input 
@@ -33,11 +57,23 @@ function ArtistForm(){
         id="artistChoice" 
         value={userInput} 
         onChange={handleChange}
-        placeholder="Taylor Swift, Ed Sheeran, Doja Cat, BTS, Lil Nas X..."
+        placeholder="Taylor Swift, Coldplay, Doja Cat, BTS, Lil Nas X..."
         />
       <button>Submit</button>
     </form>
-   
+    {/* {
+      artistTopTracks.map(topTracks => {
+        return(
+          <DisplaySoundtrack 
+            key={topTracks.mbid}
+            artist={topTracks.artist.name}
+            trackTitle={topTracks.name}
+            trackUrl={topTracks.url}
+          />
+        )
+      })
+    } */}
+   </>
   )
 }
 
--- src/QuestionForm.js
+++ src/QuestionForm.js
@@ -1,51 +1,91 @@
 // ask user a series of questions and store their responses
 import questionsData from "./questionsData";
-// import { useState } from "react";
+import { useState } from "react";
 
 function QuestionForm() {
+  const userAnswerArray = [];
   // initialize state
-  const [userAnswer, setUserAnswer] = useSate("");
+  const [userAnswer, setUserAnswer] = useState("");
+  // const [storedAnswer, setStoredAnswer] = useState([]);
 
   const handleChange = (event) => {
-    setUserAnswer(event.target.value)
+    setUserAnswer(event.target.value);
+    console.log(userAnswer)
+    userAnswerArray.push(event.target.value)
+    console.log(userAnswerArray)
   }
+  // initialize empty array for storing user answers
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(event)
+    //store user input in array
+
+    // userAnswerArray.push();
+    // console.log(userAnswer, userAnswerArray, event.target)
+  }
+ 
    
   return(
     <>
-    {
-      questionsData.map((data, index) => {
-        
-        console.log(data.question, data.answer[0], data.answer[1], data.answer[2], data.answer[3])
-        return(
-          //use info from array to populate index, question and answers
-          <form >
-            <fieldset>
-              <legend>{data.question}</legend>
-  
-              <label htmlFor="answer1">{data.answer[0]}</label>
-              <input type="radio" name="answer" id="answer1" value={data.answer[0]} checked={userAnswer === data.answer[0] ? true : false} onChange={handleChange} />
-  
-              <label htmlFor="answer2">{data.answer[1]}</label>
-              <input type="radio" name="answer" id="answer1" value={data.answer[1]} checked={userAnswer === data.answer[1] ? true : false} onChange={handleChange} />
-  
-              <label htmlFor="answer3">{data.answer[2]}</label>
-              <input type="radio" name="answer" id="answer1" value={data.answer[2]} checked={userAnswer === data.answer[2] ? true : false} onChange={handleChange} />
-  
-              <label htmlFor="answer4">{data.answer[3]}</label>
-              <input type="radio" name="answer" id="answer1" value={data.answer[3]} checked={userAnswer === data.answer[3] ? true : false} onChange={handleChange} />
-  
-              <button>Submit</button>
-  
-            </fieldset>
-          </form>
-  
-        )
+      <form onSubmit={handleSubmit}>
+        {
+          // map through questions data to populate the fieldsets
+          questionsData.map((data, index) => {
+            return(
 
-      })
-    }
+              <fieldset key={`question${index}`} id={`question${index}`}>
+                <legend>{data.question}</legend>
+
+                <label htmlFor="answer1">{data.answer[0]}</label>
+                <input 
+                  type="radio" 
+                  name="answer" 
+                  id="answer1" 
+                  value={data.answer[0]} 
+                  checked={userAnswer === data.answer[0] ? true : false} 
+                  onChange={handleChange} 
+                />
+
+                <label htmlFor="answer2">{data.answer[1]}</label>
+                <input 
+                  type="radio" 
+                  name="answer" 
+                  id="answer2" 
+                  value={data.answer[1]} 
+                  checked={userAnswer === data.answer[1] ? true : false} 
+                  onChange={handleChange} 
+                />
+
+                <label htmlFor="answer3">{data.answer[2]}</label>
+                <input 
+                  type="radio" 
+                  name="answer" 
+                  id="answer3" 
+                  value={data.answer[2]} 
+                  checked={userAnswer === data.answer[2] ? true : false} 
+                  onChange={handleChange} 
+                />
+
+                <label htmlFor="answer4">{data.answer[3]}</label>
+                <input 
+                  type="radio" 
+                  name="answer" 
+                  id="answer4" 
+                  value={data.answer[3]} 
+                  checked={userAnswer === data.answer[3] ? true : false} 
+                  onChange={handleChange} 
+                />
+             
+              </fieldset>
+            )
+          })
+        }
+        <button>Submit</button>
+      </form>
     </>
-  )
+  ) 
 }
 
 export default QuestionForm;
